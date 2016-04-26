63eff1f083056ad63db99addc189fa380f493033
An DUONG <duonghienan.dha@gmail.com>
2016-04-26 11:55:44 +0700

    add java handel

--- lita-andeptrai/lib/lita/handlers/andeptrai.rb
+++ lita-andeptrai/lib/lita/handlers/andeptrai.rb
@@ -1,12 +1,11 @@
 module Lita
   module Handlers
     class Andeptrai < Handler
-      # insert handler code here
-      route /^hello\s+(.+)/, :with_deptrai, help: { "hello An" => "reply back with hello." }
+      route /^java\s+(.+)/, :with_java, help: { "java TEXT" => "reply back with java." }
 
-      def with_deptrai( response )
+      def with_java( response )
         word = response.matches[0][0]
-        response.reply("#{word} Dep trai")
+        response.reply("!!! #{word} とジャバ !!!")
       end
       Lita.register_handler(self)
     end
