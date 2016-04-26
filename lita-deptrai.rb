890c16f03846506ef0b295f52cbee024622a69e2
An DUONG <duonghienan.dha@gmail.com>
2016-04-26 13:28:24 +0700

    add eptrai handel

---
+++ lita-deptrai.rb
@@ -0,0 +1,14 @@
+module Lita
+  module Handlers
+    class Deptrai < Handler
+      # insert handler code here
+      route /^hello\s+(.+)/, :with_deptrai, help: { "hello TEXT" => "reply back with hello." }
+
+      def with_deptrai( response )
+        word = response.matches[0][0]
+        response.reply("#{word} dep trai")
+      end
+      Lita.register_handler(self)
+    end
+  end
+end
--- lita_config.rb
+++ lita_config.rb
@@ -1,16 +1,13 @@
 Lita.configure do |config|
+  require './lita-deptrai'
   # The name your robot will use.
   config.robot.name      = "lita"
   config.robot.log_level = :info
 
-  # アダプタの指定。Slack とつなげるので、:slack と修正する
-  config.robot.adapter   = :slack
+  config.robot.adapter   = :shell
 
-  # Slack のAPIキーを指定する
-  # 後述する heoku コマンドで heroku の環境変数を設定する
   config.adapters.slack.token = ENV["SLACK_API_KEY"]
 
-  # heroku の redis アドオン Redis To Go 関連の設定
   config.redis[:url] = ENV["REDISTOGO_URL"]
   config.http.port   = ENV["PORT"]
 end
